44c70113d73679a335f95b48bbc6b889c5ff6fa7
Jeremy Cruz <jeremywcruz@gmail.com>
2023-03-11 14:20:44 -0800

    Add compression dsp

--- SimpleMBComp/Source/PluginProcessor.cpp
+++ SimpleMBComp/Source/PluginProcessor.cpp
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-        This file contains the basic framework code for a JUCE plugin processor.
+		This file contains the basic framework code for a JUCE plugin processor.
 
   ==============================================================================
 */
@@ -13,57 +13,65 @@
 //==============================================================================
 SimpleMBCompAudioProcessor::SimpleMBCompAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-    : AudioProcessor(
-          BusesProperties()
+	: AudioProcessor(
+		BusesProperties()
 #if !JucePlugin_IsMidiEffect
 #if !JucePlugin_IsSynth
-              .withInput("Input", juce::AudioChannelSet::stereo(), true)
+		.withInput("Input", juce::AudioChannelSet::stereo(), true)
 #endif
-              .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+		.withOutput("Output", juce::AudioChannelSet::stereo(), true)
 #endif
-      )
+	)
 #endif
 {
+	attack = dynamic_cast<AudioParameterFloat*>(apvts.getParameter("Attack"));
+	jassert(attack != nullptr);
+	release = dynamic_cast<AudioParameterFloat*>(apvts.getParameter("Release"));
+	jassert(release != nullptr);
+	threshold = dynamic_cast<AudioParameterFloat*>(apvts.getParameter("Threshold"));
+	jassert(threshold != nullptr);
+	ratio = dynamic_cast<AudioParameterChoice*>(apvts.getParameter("Ratio"));
+	jassert(ratio != nullptr);
 }
 
 SimpleMBCompAudioProcessor::~SimpleMBCompAudioProcessor() {}
 
 //==============================================================================
 const juce::String SimpleMBCompAudioProcessor::getName() const {
-  return JucePlugin_Name;
+	return JucePlugin_Name;
 }
 
 bool SimpleMBCompAudioProcessor::acceptsMidi() const {
 #if JucePlugin_WantsMidiInput
-  return true;
+	return true;
 #else
-  return false;
+	return false;
 #endif
 }
 
 bool SimpleMBCompAudioProcessor::producesMidi() const {
 #if JucePlugin_ProducesMidiOutput
-  return true;
+	return true;
 #else
-  return false;
+	return false;
 #endif
 }
 
 bool SimpleMBCompAudioProcessor::isMidiEffect() const {
 #if JucePlugin_IsMidiEffect
-  return true;
+	return true;
 #else
-  return false;
+	return false;
 #endif
 }
 
 double SimpleMBCompAudioProcessor::getTailLengthSeconds() const { return 0.0; }
 
 int SimpleMBCompAudioProcessor::getNumPrograms() {
-  return 1;  // NB: some hosts don't cope very well if you tell them there are 0
-             // programs,
-  // so this should be at least 1, even if you're not really implementing
-  // programs.
+	return 1;  // NB: some hosts don't cope very well if you tell them there are 0
+			   // programs,
+	// so this should be at least 1, even if you're not really implementing
+	// programs.
 }
 
 int SimpleMBCompAudioProcessor::getCurrentProgram() { return 0; }
@@ -71,17 +79,24 @@ int SimpleMBCompAudioProcessor::getCurrentProgram() { return 0; }
 void SimpleMBCompAudioProcessor::setCurrentProgram(int index) {}
 
 const juce::String SimpleMBCompAudioProcessor::getProgramName(int index) {
-  return {};
+	return {};
 }
 
 void SimpleMBCompAudioProcessor::changeProgramName(
-    int index, const juce::String& newName) {}
+	int index, const juce::String& newName) {}
 
 //==============================================================================
 void SimpleMBCompAudioProcessor::prepareToPlay(double sampleRate,
-                                               int samplesPerBlock) {
-  // Use this method as the place to do any pre-playback
-  // initialisation that you need..
+	int samplesPerBlock) {
+// Use this method as the place to do any pre-playback
+// initialisation that you need..
+	ProcessSpec spec;
+
+	spec.maximumBlockSize = samplesPerBlock;
+	spec.numChannels = getTotalNumOutputChannels();
+	spec.sampleRate = sampleRate;
+
+	compressor.prepare(spec);
 }
 
 void SimpleMBCompAudioProcessor::releaseResources() {
@@ -91,110 +106,115 @@ void SimpleMBCompAudioProcessor::releaseResources() {
 
 #ifndef JucePlugin_PreferredChannelConfigurations
 bool SimpleMBCompAudioProcessor::isBusesLayoutSupported(
-    const BusesLayout& layouts) const {
+	const BusesLayout& layouts) const {
 #if JucePlugin_IsMidiEffect
-  juce::ignoreUnused(layouts);
-  return true;
+	juce::ignoreUnused(layouts);
+	return true;
 #else
   // This is the place where you check if the layout is supported.
   // In this template code we only support mono or stereo.
   // Some plugin hosts, such as certain GarageBand versions, will only
   // load plugins that support stereo bus layouts.
-  if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
-      layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-    return false;
+	if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
+		layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+		return false;
 
-    // This checks if the input layout matches the output layout
+		// This checks if the input layout matches the output layout
 #if !JucePlugin_IsSynth
-  if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-    return false;
+	if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+		return false;
 #endif
 
-  return true;
+	return true;
 #endif
 }
 #endif
 
 void SimpleMBCompAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer,
-                                              juce::MidiBuffer& midiMessages) {
-  juce::ScopedNoDenormals noDenormals;
-  auto totalNumInputChannels = getTotalNumInputChannels();
-  auto totalNumOutputChannels = getTotalNumOutputChannels();
+	juce::MidiBuffer& midiMessages) {
+	juce::ScopedNoDenormals noDenormals;
+	auto totalNumInputChannels = getTotalNumInputChannels();
+	auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-  // In case we have more outputs than inputs, this code clears any output
-  // channels that didn't contain input data, (because these aren't
-  // guaranteed to be empty - they may contain garbage).
-  // This is here to avoid people getting screaming feedback
-  // when they first compile a plugin, but obviously you don't need to keep
-  // this code if your algorithm always overwrites all the output channels.
-  for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-    buffer.clear(i, 0, buffer.getNumSamples());
+	// In case we have more outputs than inputs, this code clears any output
+	// channels that didn't contain input data, (because these aren't
+	// guaranteed to be empty - they may contain garbage).
+	// This is here to avoid people getting screaming feedback
+	// when they first compile a plugin, but obviously you don't need to keep
+	// this code if your algorithm always overwrites all the output channels.
+	for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+		buffer.clear(i, 0, buffer.getNumSamples());
 
-  // This is the place where you'd normally do the guts of your plugin's
-  // audio processing...
-  // Make sure to reset the state if your inner loop is processing
-  // the samples and the outer loop is handling the channels.
-  // Alternatively, you can process the samples with the channels
-  // interleaved by keeping the same state.
-  for (int channel = 0; channel < totalNumInputChannels; ++channel) {
-    auto* channelData = buffer.getWritePointer(channel);
+	compressor.setAttack(attack->get());
+	compressor.setRelease(release->get());
+	compressor.setThreshold(threshold->get());
+	compressor.setRatio(ratio->getCurrentChoiceName().getFloatValue());
 
-    // ..do something to the data...
-  }
+	auto block = AudioBlock<float>(buffer);
+	auto context = ProcessContextReplacing<float>(block);
+
+	compressor.process(context);
 }
 
 //==============================================================================
 bool SimpleMBCompAudioProcessor::hasEditor() const {
-  return true;  // (change this to false if you choose to not supply an editor)
+	return true;  // (change this to false if you choose to not supply an editor)
 }
 
 juce::AudioProcessorEditor* SimpleMBCompAudioProcessor::createEditor() {
   // return new SimpleMBCompAudioProcessorEditor(*this);
-  return new GenericAudioProcessorEditor(*this);
+	return new GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
 void SimpleMBCompAudioProcessor::getStateInformation(
-    juce::MemoryBlock& destData) {
+	juce::MemoryBlock& destData) {
   // You should use this method to store your parameters in the memory block.
   // You could do that either as raw data, or use the XML or ValueTree classes
   // as intermediaries to make it easy to save and load complex data.
+	MemoryOutputStream mos(destData, true);
+	apvts.state.writeToStream(mos);
 }
 
 void SimpleMBCompAudioProcessor::setStateInformation(const void* data,
-                                                     int sizeInBytes) {
-  // You should use this method to restore your parameters from this memory
-  // block, whose contents will have been created by the getStateInformation()
-  // call.
+	int sizeInBytes) {
+// You should use this method to restore your parameters from this memory
+// block, whose contents will have been created by the getStateInformation()
+// call.
+	auto tree = ValueTree::readFromData(data, sizeInBytes);
+	if (tree.isValid())
+	{
+		apvts.replaceState(tree);
+	}
 }
 
 juce::AudioProcessorValueTreeState::ParameterLayout
 SimpleMBCompAudioProcessor::createParameterLayout() {
-  AVPTS::ParameterLayout layout;
-  layout.add(std::make_unique<AudioParameterFloat>(
-      "Threshold", "Threshold", NormalisableRange<float>(-60.f, 12.f, 1.f, 1.f),
-      0.f));
+	APVTS::ParameterLayout layout;
+	layout.add(std::make_unique<AudioParameterFloat>(
+		"Threshold", "Threshold", NormalisableRange<float>(-60.f, 12.f, 1.f, 1.f),
+		0.f));
 
-  auto attackReleaseRange = NormalisableRange<float>(5, 500, 1, 1);
-  layout.add(std::make_unique<AudioParameterFloat>("Attack", "Attack",
-                                                   attackReleaseRange, 50.f));
-  layout.add(std::make_unique<AudioParameterFloat>("Release", "Release",
-                                                   attackReleaseRange, 250.f));
+	auto attackReleaseRange = NormalisableRange<float>(5, 500, 1, 1);
+	layout.add(std::make_unique<AudioParameterFloat>("Attack", "Attack",
+		attackReleaseRange, 50.f));
+	layout.add(std::make_unique<AudioParameterFloat>("Release", "Release",
+		attackReleaseRange, 250.f));
 
-  auto ratios =
-      std::vector<double>{1, 1.5, 2, 3, 4, 5, 6, 7, 8, 10, 15, 20, 50, 100};
+	auto ratios =
+		std::vector<double>{ 1, 1.5, 2, 3, 4, 5, 6, 7, 8, 10, 15, 20, 50, 100 };
 
-  juce::StringArray sa;
-  for (auto ratio : ratios) {
-    sa.add(juce::String(ratio, 1));
-  }
+	juce::StringArray sa;
+	for (auto ratio : ratios) {
+		sa.add(juce::String(ratio, 1));
+	}
 
-  layout.add(std::make_unique<AudioParameterChoice>("Ratio", "Ratio", sa, 2));
-  return layout;
+	layout.add(std::make_unique<AudioParameterChoice>("Ratio", "Ratio", sa, 2));
+	return layout;
 }
 
 //==============================================================================
 // This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() {
-  return new SimpleMBCompAudioProcessor();
+	return new SimpleMBCompAudioProcessor();
 }
